cd807e55cdd07eba4f00a11b6048cd9a727534c3
Tushar Kumar <93111021+tusharkumarthakur@users.noreply.github.com>
2022-05-30 07:45:47 -0700

    Create Q15.py

---
+++ 20 questions/Q15.py
@@ -0,0 +1,15 @@
+#Finding the log of a number
+import math
+ 
+ 
+# returning the log of 2,3
+print ("The value of log 2 with base 3 is : ", end="")
+print (math.log(2,3))
+ 
+# returning the log2 of 16
+print ("The value of log2 of 16 is : ", end="")
+print (math.log2(16))
+    
+# returning the log10 of 10000
+print ("The value of log10 of 10000 is : ", end="")
+print (math.log10(10000))
