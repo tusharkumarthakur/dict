12985457e2fe1f03f34881d02767e2daca4e88c4
Tushar Kumar <93111021+tusharkumarthakur@users.noreply.github.com>
2022-05-30 07:47:19 -0700

    Create Q18.py

---
+++ 20 questions/Q18.py
@@ -0,0 +1,14 @@
+#Selecting random elements from the list, string, and tuple
+import random
+  
+# prints a random value from the list
+list1 = [1, 2, 3, 4, 5, 6]
+print(random.choice(list1))
+  
+# prints a random item from the string
+string = "geeks"
+print(random.choice(string))
+  
+# prints a random item from the tuple
+tuple1 = (1, 2, 3, 4, 5)
+print(random.choice(tuple1)
