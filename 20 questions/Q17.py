bc5f8c65b3771994c7570ce153a4bc5f31dc2dcd
Tushar Kumar <93111021+tusharkumarthakur@users.noreply.github.com>
2022-05-30 07:46:44 -0700

    Create Q17.py

---
+++ 20 questions/Q17.py
@@ -0,0 +1,12 @@
+#Creating random integers
+import random
+  
+# Generates a random number between
+# a given positive range
+r1 = random.randint(5, 15)
+print("Random number between 5 and 15 is % s" % (r1))
+  
+# Generates a random number between
+# two given negative range
+r2 = random.randint(-10, -2)
+print("Random number between -10 and -2 is % d" % (r2))
